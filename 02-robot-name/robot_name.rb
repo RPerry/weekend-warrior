24e8aa9cbf9b35847e61b3a72c33958d7c52342a
Riyo <riyo.perry@gmail.com>
2019-03-03 16:08:29 -0800

    finished robot_name

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,20 @@
 
+class Robot
+  attr_reader :name
+
+  def initialize
+    number = (0..9).to_a.sample(3)
+    letters = ("A".."Z").to_a.sample(2)
+    @name = (letters + number).join
+  end
+
+  def bootup
+    return "Hello, I'm #{@name}"
+  end
+
+  def reset
+    number = (0..9).to_a.sample(3)
+    letters = ("A".."Z").to_a.sample(2)
+    @name = (letters + number).join
+  end
+end
